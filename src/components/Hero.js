10e015c49375c9fd188391180538be57af90f8fe
“Eliza <et65@rice.edu>
2025-01-29 17:58:05 -0600

    feat: Updated the Home page according to design

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,28 +5,23 @@ import defaultImg from "../images/home-page-bcg.jpg";
 // Styled components
 const HeroContainer = styled.div`
   position: relative;
-  min-height: calc(100vh - 66px);
+  min-height: calc(80vh - 66px);
   display: flex;
   align-items: center;
   justify-content: center;
-  overflow: hidden;
 `;
 
 const HeroImage = styled.img`
   position: absolute;
-  top: 0;
-  left: 0;
   width: 100%;
   height: 100%;
   object-fit: cover;
-  z-index: -1;
 `;
 
 const HeroContent = styled.div`
   z-index: 1;
   text-align: center;
   color: var(--mainWhite);
-  padding: 2rem;
 `;
 
 function Hero({ children }) {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,52 @@ import styled from "styled-components";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
 import FeaturedRooms from "../components/FeaturedRooms";
+import { Button, Box, Typography } from "@mui/material";
 
 // Styled components
 const HomeContainer = styled.main`
   display: flex;
   flex-direction: column;
 `;
+
+const ButtonContainer = styled(Box)`
+  display: flex;
+  justify-content: center;
+  gap: 6rem;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+
+  @media screen and (max-width: 767px) {
+    gap: 2rem;
+  }
+`;
+
+const CustomButton = styled(Button)`
+  border: 2px solid #bdbdbd;
+  border-radius: 50px !important;
+  padding: 12px 32px;
+  width: 15rem;
+  height: 4rem;
+  text-transform: none !important;
+  font-size: 1.1rem !important;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+
+  @media screen and (max-width: 767px) {
+    width: 7rem;
+    height: 2.5rem;
+    font-size: 0.7rem !important;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1025px){
+    width: 10rem;
+    height: 3rem;
+    font-size: 1rem !important;
+  }
+`;
+
 
 function Home() {
   useEffect(() => {
@@ -17,7 +57,12 @@ function Home() {
 
   return (
     <React.Fragment>
-      <Hero />
+      <Hero/>
+      <ButtonContainer>
+        <CustomButton variant="outlined">How Reserve a Classroom</CustomButton>
+        <CustomButton variant="outlined">Classroom Training</CustomButton>
+        <CustomButton variant="outlined">Classroom Help</CustomButton>
+      </ButtonContainer>
       <HomeContainer>
         <Services />
         <FeaturedRooms />
